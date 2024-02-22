eea21ed2f00e8ec390f70590bae581a2ae8fa1e8
Juho Simojoki <juho.simojoki@outlook.com>
2024-02-22 14:18:48 +0200

    Added another widget

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
         home: MyHomePage(),
       ),
@@ -43,7 +43,64 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+      case 1:
+        page = Favorites();
+      default:
+        throw UnimplementedError('No widget for selected index');
+    }
+    return LayoutBuilder(builder: (
+      context,
+      constraints,
+    ) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: constraints.maxWidth >= 600,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                      icon: Icon(Icons.favorite), label: Text('Faorites'))
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            )
+          ],
+        ),
+      );
+    });
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
@@ -117,6 +174,33 @@ class BigCard extends StatelessWidget {
           semanticsLabel: '${pair.first} ${pair.second}',
         ),
       ),
+    );
+  }
+}
+
+class Favorites extends StatelessWidget {
+  const Favorites({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var pairs = context.watch<MyAppState>().favorites;
+
+    if (pairs.isEmpty) {
+      return (Text('No word pairs to show yet'));
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Text('Word pairs:'),
+        ),
+        for (var pair in pairs)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
     );
   }
 }
